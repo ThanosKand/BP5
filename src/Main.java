507ac2611f98eb2577f621efdf96def6df1cd15d
Athanasios Kandylas <kand-2@hotmail.com>
2019-09-16 10:50:50 +0200

    Ask your name

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         System.out.println("Joy");
 
         System.out.println("How old are you?");
         System.out.println("Do you miss our walks?");
+
+        System.out.println("What's your name?");
+        String name=in.next();
+        System.out.println(name);
     }
 }
