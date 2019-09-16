9926b35b3e90b977326239d22ac85cb8b42cd917
Athanasios Kandylas <kand-2@hotmail.com>
2019-09-16 10:45:19 +0200

    Add Questions

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Joy");
+
+        System.out.println("How old are you?");
+        System.out.println("Do you miss our walks?");
     }
 }
